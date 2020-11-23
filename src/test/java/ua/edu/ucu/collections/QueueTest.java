3983205981f370f0ef26f16a8c288e572e9c4820
Sonya Bas <60771112+Nedeja@users.noreply.github.com>
2020-11-23 19:36:26 +0200

    Update QueueTest.java

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,45 @@
 package ua.edu.ucu.collections;
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class QueueTest {
+    private Queue q;
+
+    @Before
+    public void setUp() {
+        q = new Queue();
+    }
     
     @Test
-    public void testSomeMethod() {
+    public void testQueueDifferent() {
+        int[] elems = new int[] {1, 2, 3};
+        for (int i : elems) {
+            q.enqueue(i);
+        }
+        for (int i = 0; i < 3; i++) {
+            assertEquals(elems[i], q.peek());
+            assertEquals(elems[i], q.dequeue());
+        }
+    }
+
+    @Test
+    public void testQueueSimilar() {
+        int[] elements = new int[]{1, 1, 1};
+        for (int i : elements) {
+            q.enqueue(i);
+        }
+        for (int i = 0; i < 3; i++) {
+            assertEquals(elements[i], q.peek());
+            assertEquals(elements[i], q.dequeue());
+        }
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testQueueError() {
+        q.dequeue();
     }
     
 }
+
