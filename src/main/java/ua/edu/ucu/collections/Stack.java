519893102c39708b2c1382ab8799cad91dbf3eb7
Sonya Bas <60771112+Nedeja@users.noreply.github.com>
2020-11-23 19:32:46 +0200

    Update Stack.java

--- src/main/java/ua/edu/ucu/collections/Stack.java
+++ src/main/java/ua/edu/ucu/collections/Stack.java
@@ -1,5 +1,26 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Stack {
-    
+    private ImmutableLinkedList lst;
+
+    public Stack() {
+        lst = new ImmutableLinkedList();
+    }
+
+    public Object peek() {
+        return this.lst.getFirst();
+    }
+
+    public Object pop() {
+        Object element = this.lst.getFirst();
+        this.lst = this.lst.removeFirst();
+        return element;
+    }
+
+    public void push(Object element) {
+        this.lst = this.lst.addFirst(element);
+    }
 }
+
