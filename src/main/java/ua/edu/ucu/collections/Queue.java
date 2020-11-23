bd120c1734ac199fb05000399db0f47de0a45d9a
Sonya Bas <60771112+Nedeja@users.noreply.github.com>
2020-11-23 19:32:11 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,25 @@
 package ua.edu.ucu.collections;
 
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
 public class Queue {
-    
+    private ImmutableLinkedList lst;
+
+    public Queue() {
+        lst = new ImmutableLinkedList();
+    }
+
+    public Object peek() {
+        return this.lst.getFirst();
+    }
+
+    public Object dequeue() {
+        Object element = this.lst.getFirst();
+        this.lst = this.lst.removeFirst();
+        return element;
+    }
+
+    public void enqueue(Object element) {
+        lst = lst.addLast(element);
+    }
 }
